456dd6e63ed38cd2e7f2a0a7745e970534343e8a
alexeev509 <32396885+alexeev509@users.noreply.github.com>
2020-12-06 23:50:42 +0300

    Create A.cpp

---
+++ competition/A.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+	std::cout.sync_with_stdio(false);
+	std::cin.sync_with_stdio(false);
+	int n;
+	std::cin >> n;
+
+	std::cout << n * 10;
+}
